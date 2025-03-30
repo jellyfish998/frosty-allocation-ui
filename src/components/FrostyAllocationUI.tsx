cf751b65ce622f34f1e9b2470510951d052daf04
jellyfish998 <jellyfish98@gmail.com>
2025-03-30 00:11:15 -0500

    update

---
+++ src/components/FrostyAllocationUI.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FrostyAllocationUI() {
+  return (
+    <div className="text-center text-2xl font-semibold mt-10">
+      Frosty Allocation UI is up and running!
+    </div>
+  )
+}
